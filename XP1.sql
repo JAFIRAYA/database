eb07880012017dbee30abba11f5d4a39e8ca56e6
JAFIRAYA <jafiraya.2004@gmail.com>
2023-06-07 19:47:48 +0200

    daily

--- XP1.sql
+++ XP1.sql
@@ -26,3 +26,21 @@ VALUES
   SELECT * FROM customers WHERE last_name = 'Smith';
   SELECT * FROM customers WHERE last_name = 'Jones';
   SELECT * FROM customers WHERE first_name <> 'Scott';
+  
+  
+  
+  ->dailyChallenge
+  
+  CREATE TABLE actors (
+  id INT ,
+  first_name VARCHAR(50),
+  last_name VARCHAR(50),
+  age INT
+);
+INSERT INTO actors (id, first_name, last_name, age)
+VALUES (1, 'Tom', 'Hanks', 65),
+       (2, 'Meryl', 'Streep', 72),
+	   (3, 'Leonardo', 'DiCaprio', 46)
+SELECT COUNT(*) FROM actors;
+INSERT INTO actors (id, name, age) VALUES (4, '', NULL);
+->receive an error message indicating the constraint violation.
